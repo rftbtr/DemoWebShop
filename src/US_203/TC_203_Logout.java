21c17f8fc26e2dbba00deb5cbc0cbf0521f94d83
rftbtr <rifatbatir@gmail.com>
2025-03-24 23:39:00 +0300

    Added actions method to TC_203_Logout and TC_204_Login.

--- src/US_203/TC_203_Logout.java
+++ src/US_203/TC_203_Logout.java
@@ -18,28 +18,29 @@ public class TC_203_Logout extends BaseDriver {
         Assert.assertEquals("The website could not be accessed...", "https://demowebshop.tricentis.com/", driver.getCurrentUrl());
 
         WebElement login1Button = driver.findElement(By.className("ico-login"));
-        login1Button.click();
+        actions.moveToElement(login1Button).click().build().perform();
         MyFunc.sleep(2);
         Assert.assertEquals("Login page could not be reached.", "https://demowebshop.tricentis.com/login", driver.getCurrentUrl());
 
         WebElement emailField = driver.findElement(By.id("Email"));
-        emailField.sendKeys(email);
+        actions.moveToElement(emailField).click().sendKeys(email).build().perform();
         Assert.assertEquals("Inconsistent email address.", "rfttest@gmail.com", emailField.getAttribute("value"));
 
         WebElement passwordField = driver.findElement(By.id("Password"));
-        passwordField.sendKeys(password);
+        actions.moveToElement(passwordField).click().sendKeys(password).build().perform();
 
         WebElement login2Button = driver.findElement(By.xpath("//*[contains(@class,'button-1 login-button')]"));
-        login2Button.click();
+        actions.moveToElement(login2Button).click().build().perform();
         MyFunc.sleep(2);
         Assert.assertEquals("Failed to return to homepage...", "https://demowebshop.tricentis.com/", driver.getCurrentUrl());
 
         WebElement userCheck = driver.findElement(By.xpath("//a[text()='rfttest@gmail.com']"));
         Assert.assertEquals("User not verified!", email, userCheck.getText());
-        System.out.println("LOGIN SUCCESS");
+        System.out.println("LogIn SUCCESS");
 
         WebElement logoutButton = driver.findElement(By.className("ico-logout"));
-        logoutButton.click();
+        actions.moveToElement(logoutButton).click().build().perform();
+        MyFunc.sleep(2);
         WebElement checkLogout = driver.findElement(By.className("ico-register"));
         Assert.assertTrue(checkLogout.isDisplayed());
         System.out.println("LogOut REALIZED");
